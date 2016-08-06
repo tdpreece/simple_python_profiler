f5053120697e829365e12a0c1da47a63efa497c2
Tim Preece <tdpreece@gmail.com>
2016-08-06 17:03:18 +0100

    Write stats to specified dir.

--- src/profiler.py
+++ src/profiler.py
@@ -1,9 +1,10 @@
+import os
 import cProfile
 
 
 class profile(object):
-    def __init__(self):
-        pass
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
 
     def __call__(self, func):
         def _inner(*args, **kwargs):
@@ -11,7 +12,7 @@ class profile(object):
             pr.enable()
             return_value = func(*args, **kwargs)
             pr.disable()
-            stats_file = 'stats'
+            stats_file = os.path.join(self.output_dir, 'stats')
             pr.dump_stats(stats_file)
             return return_value
         return _inner
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
-from unittest import expectedFailure, TestCase
+import shutil
+from unittest import TestCase
 
 from mock import MagicMock, sentinel
 
@@ -9,19 +10,21 @@ from profiler import profile
 
 class TestProfileDecorator(TestCase):
     def setUp(self):
+        self.dir_path = tempfile.mkdtemp()
+        self.addCleanup(lambda: shutil.rmtree(self.dir_path))
         self.profiled_function = MagicMock(return_value=sentinel.return_value)
+        self.profile_decorator = profile(self.dir_path)
+        self.decorated_function = self.profile_decorator(
+            self.profiled_function
+        )
 
     def test_profiled_function_with_no_args_is_executed(self):
-        profile_decorator = profile()
-        decorated_function = profile_decorator(self.profiled_function)
-        decorated_function()
+        self.decorated_function()
         self.profiled_function.assert_called_once_with()
 
     def test_profiled_function_with_args_is_executed(self):
         args = (sentinel.arg1, sentinel.arg2)
-        profile_decorator = profile()
-        decorated_function = profile_decorator(self.profiled_function)
-        decorated_function(*args)
+        self.decorated_function(*args)
         self.profiled_function.assert_called_once_with(*args)
 
     def test_profiled_function_with_kwargs_is_executed(self):
@@ -29,20 +32,19 @@ class TestProfileDecorator(TestCase):
             'key1': sentinel.value1,
             'key2': sentinel.value2
         }
-        profile_decorator = profile()
-        decorated_function = profile_decorator(self.profiled_function)
-        decorated_function(**kwargs)
+        self.decorated_function(**kwargs)
         self.profiled_function.assert_called_once_with(**kwargs)
 
     def test_returns_returned_variables_of_profiled_function(self):
-        profile_decorator = profile()
-        decorated_function = profile_decorator(self.profiled_function)
-        return_value = decorated_function()
+        return_value = self.decorated_function()
         expected_return_value = self.profiled_function()
         self.assertEqual(return_value, expected_return_value)
 
-    @expectedFailure
     def test_stats_file_is_printed(self):
-        dir_path = tempfile.mkdtemp()
-        self.addCleanup(lambda: os.rmdir(dir_path))
-        raise NotImplementedError()
+        stats_files_before = os.listdir(self.dir_path)
+        self.assertEqual(len(stats_files_before), 0)
+
+        self.decorated_function()
+
+        stats_files_after = os.listdir(self.dir_path)
+        self.assertEqual(len(stats_files_after), 1)
