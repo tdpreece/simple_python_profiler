70e1b5355c0ce0ca65b6fb4dcee1a4896cc21e7f
Tim Preece <tdpreece@gmail.com>
2016-08-06 16:42:19 +0100

    Refactor decorator so it can take args.

--- src/profiler.py
+++ src/profiler.py
@@ -2,14 +2,16 @@ import cProfile
 
 
 class profile(object):
-    def __init__(self, func):
-        self.func = func
+    def __init__(self):
+        pass
 
-    def __call__(self, *args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
-        return_value = self.func(*args, **kwargs)
-        pr.disable()
-        stats_file = 'stats'
-        pr.dump_stats(stats_file)
-        return return_value
+    def __call__(self, func):
+        def _inner(*args, **kwargs):
+            pr = cProfile.Profile()
+            pr.enable()
+            return_value = func(*args, **kwargs)
+            pr.disable()
+            stats_file = 'stats'
+            pr.dump_stats(stats_file)
+            return return_value
+        return _inner
--- tests.py
+++ tests.py
@@ -12,14 +12,14 @@ class TestProfileDecorator(TestCase):
         self.profiled_function = MagicMock(return_value=sentinel.return_value)
 
     def test_profiled_function_with_no_args_is_executed(self):
-        profile_decorator = profile
+        profile_decorator = profile()
         decorated_function = profile_decorator(self.profiled_function)
         decorated_function()
         self.profiled_function.assert_called_once_with()
 
     def test_profiled_function_with_args_is_executed(self):
         args = (sentinel.arg1, sentinel.arg2)
-        profile_decorator = profile
+        profile_decorator = profile()
         decorated_function = profile_decorator(self.profiled_function)
         decorated_function(*args)
         self.profiled_function.assert_called_once_with(*args)
@@ -29,13 +29,13 @@ class TestProfileDecorator(TestCase):
             'key1': sentinel.value1,
             'key2': sentinel.value2
         }
-        profile_decorator = profile
+        profile_decorator = profile()
         decorated_function = profile_decorator(self.profiled_function)
         decorated_function(**kwargs)
         self.profiled_function.assert_called_once_with(**kwargs)
 
     def test_returns_returned_variables_of_profiled_function(self):
-        profile_decorator = profile
+        profile_decorator = profile()
         decorated_function = profile_decorator(self.profiled_function)
         return_value = decorated_function()
         expected_return_value = self.profiled_function()
