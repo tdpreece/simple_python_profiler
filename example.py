8b9a2ae7825b0549388dc4f0b7165aa00e1312f1
Tim Preece <tdpreece@gmail.com>
2016-08-06 15:03:59 +0100

    Extract profiling into a decorator.

--- example.py
+++ example.py
@@ -2,26 +2,31 @@ import cProfile
 import time
 
 
+def profile(func):
+    def _inner():
+        pr = cProfile.Profile()
+        pr.enable()
+        func()
+        pr.disable()
+        stats_file = 'stats'
+        pr.dump_stats(stats_file)
+    return _inner
+
+
 def a_function():
-    time.sleep(1)
+    time.sleep(0.1)
 
 
 def another_function():
-    time.sleep(1)
+    time.sleep(0.1)
 
 
+@profile
 def an_example():
-    profiler = cProfile.Profile()
-    profiler.enable()
-
-    for i in range(2):
+    for i in range(3):
         a_function()
     for j in range(2):
         another_function()
-
-    profiler.disable()
-    stats_file = 'stats'
-    profiler.dump_stats(stats_file)
 
 
 if __name__ == '__main__':
